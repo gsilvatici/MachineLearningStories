98b8527d8cf4094a179db083176531bd10137306
Gabriel Silvatici <gsilvati@itba.edu.ar>
2021-09-29 08:36:04 -0300

    last commit aux

--- tp2/src/aux_functions.py
+++ tp2/src/aux_functions.py
@@ -22,8 +22,8 @@ def plot_matrix(matrix, filename):
         discr = matrix[row][col]
         ax.text(row, col, str(discr), va='center', ha='center')
 
-    plt.xlabel("Estimacion")
-    plt.ylabel("Esperado")
+    # plt.xlabel("Estimacion")
+    # plt.ylabel("Real")
     plt.savefig(f'{filename}', bbox_inches='tight')
     plt.close()
 
@@ -31,18 +31,18 @@ def plot_precision(precisions_knn, precisions_weight_knn, k, filename):
 
     plt.title(f'Precisión con KNN pesado y k={k}')
     plt.ylabel('Precisión')
-    plt.xlabel('Iteración')
+    plt.xlabel('Iteración del bloque cruzado')
 
     points = np.arange(len(precisions_weight_knn))
     width = 0.8
 
-    plt.bar(points, precisions_weight_knn, width, color='blue')
+    plt.bar(points, precisions_weight_knn, width, label='KNN pesado', color='blue')
 
     plt.xticks(points)
-    plt.legend(loc='lower left')
+    # plt.legend(loc='upper left')
 
-    plt.show()
-    plt.savefig(f'{filename}', bbox_inches='tight')
+    # plt.show()
+    plt.savefig(f'{filename}')
     plt.close()
 
 
@@ -63,3 +63,43 @@ def euclidean_distance_from(x1):
         return math.sqrt(distance)
     return euclidean_distance
 
+def plot_precision_k(knn_means, w_knn_means, labels, filename):
+
+    points = np.arange(len(knn_means))
+
+    # plt.title('Precision obtenida sobre distintos K de validacion cruzada' )
+    # plt.ylabel('Precisión')
+    # plt.xlabel('K de validacion cruzada')
+    # plt.xticks(points, labels)
+
+
+    # points = np.arange(len(knn_means))
+    # width = 0.8
+
+    # plt.bar(points, knn_means, 0.8, color='blue',label='KNN means')
+
+    # plt.bar(points, weight_knn_means, 0.5, color='red', label='Weighted KNN means')
+
+    # plt.errorbar(
+    #     points, knn_means, knn_stds, linestyle='None', label='KNN', capsize=5, marker='o', color='blue'
+    # )
+    # plt.errorbar(
+    #     points, w_knn_means, w_knn_stds, linestyle='None', label='Weighted KNN', capsize=5, marker='o', color='plum'
+    # )
+    # plt.legend()
+
+    # plt.savefig(f'{filename}', bbox_inches='tight')
+    # plt.clf()
+
+    plt.title('Precision obtenida sobre distintos K de validacion cruzada' )
+    plt.ylabel('Precisión')
+    plt.xlabel('K de validacion cruzada')
+
+    plt.plot(knn_means, label='KNN', color='red')
+    plt.plot(w_knn_means,  label='KNN pesado', color='blue')
+    plt.xticks(points, labels)
+    plt.legend()
+
+    # filename = filename.split('.')[0] + '_only_means.png'
+    plt.savefig(f'{filename}')
+    plt.close()
